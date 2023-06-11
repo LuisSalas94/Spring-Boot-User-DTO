5b44892e0a71488a80af7b603870f8f1d2cbeabd
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-10 21:54:07 -0500

    Create User Entity

---
+++ src/main/java/com/userdemo/dto/entity/User.java
@@ -0,0 +1,23 @@
+package com.userdemo.dto.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
