1c88de29199b006acfbafaee6c24b4cc39e930ee
Fernando Salas <luisfernandosalasg@gmail.com>
2023-06-10 22:56:39 -0500

    Create DTO class

---
+++ src/main/java/com/userdemo/dto/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.userdemo.dto.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
